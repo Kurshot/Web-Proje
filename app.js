e3e78f0979a689624c0ffd52e650000a0aca1443
Kursat <sonkurahmetkursat@gmail.com>
2022-04-21 22:39:16 +0300

    update

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-let isim, mail, metin = false;
+let isim, mail, metin, checkbox = false;
 
-function checkValidName(){
+// Ad Soyad kontrolü
+
+function checkValidName(){ 
     let text = "";
     let x = document.querySelector("#inputIsim").value;
-    if(x === ''){
+    if(x === '' || x.trim().length === 0){
         text = "Ad Soyad kısmı boş bırakılamaz";
+        isim = false;
     }
     else
         isim = true;
     document.querySelector(".hataBilgisiIsim").innerHTML = text;
 }
 
+// Mail adresi kontrolü
 
 function checkValidMail(){
     let text = "";
     let x = document.querySelector("#inputMail").value;
-    if(x === ''){
+    if(x === '' || x.trim().length === 0){
         text = "Mail adresi kısmı boş bırakılamaz."
+        mail = false;
     }
     else{ // Mail kısmı boş bırakılmamış ise
         let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -24,11 +29,27 @@ function checkValidMail(){
             mail = true;
             text = "";
         }
-        else
+        else{
             text = "Lütfen e-mail formatında girdi giriniz."
+            mail = false;
+        }
+            
     }
-    document.querySelector(".hataBilgisiMail").innerHTML = text;
-    
+    document.querySelector(".hataBilgisiMail").innerHTML = text;  
+}
+
+// Textarea kontrolü
+
+function checkValidText(){
+    let text = "";
+    let x = document.querySelector("#inputText").value;
+    if(x === '' || x.trim().length === 0){
+        text = "Mesaj kısmı boş bırakılamaz.";
+        metin = false;
+    }
+    else
+        metin = true;
+    document.querySelector(".hataBilgisiMetin").innerHTML = text;
 }
 
 let button = document.getElementById("submitButton");
@@ -37,7 +58,8 @@ button.addEventListener('click',function (e)
 {
     checkValidName();
     checkValidMail();
-    if( isim === false || mail === false || metin === false)
+    checkValidText();
+    if( !(isim && mail && metin) )
         e.preventDefault();
     
 });
