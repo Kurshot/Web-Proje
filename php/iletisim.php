ff66dca8fc4bca5da0e436dedc88d12553555f62
Kursat <sonkurahmetkursat@gmail.com>
2022-04-23 17:31:17 +0300

    update

--- php/iletisim.php
+++ php/iletisim.php
@@ -21,7 +21,7 @@
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link" href="">Şehrim</a>
+                    <a class="nav-link" href="../sehrim.html">Şehrim</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="../iletisim.html">İletişim</a>
@@ -84,7 +84,7 @@
       '
     
     
-    ?>
+      ?>
     </div>
 
     
--- php/login.php
+++ php/login.php
@@ -5,23 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/style.css">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
     <header class="sticky-top">
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #99B6C6;">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #99B6C6">
             <div class="container-fluid">
-              <a class="navbar-brand" href="index.html">Anasayfa</a>
+              <a class="navbar-brand" href="../index.html">Anasayfa</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link" href="">Şehrim</a>
+                    <a class="nav-link" href="../sehrim.html">Şehrim</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="../iletisim.html">İletişim</a>
@@ -40,58 +39,40 @@
           </nav>
     </header>
 
+    <section class="gradient-custom">
+        <div class="container py-5 mt-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card text-white" style="background-color: #93B1C1; border-radius: 1rem;">
+                <div class="card-body p-5 text-left">
+                  <div class="mb-md-5 mt-md-4 pb-5">
+                    <?php
 
-    <section class="vh-100 gradient-custom">
-      <div class="container col-md-6">
-          <h1 class="display-5">İletişim</h1>
-          <form action="php/iletisim.php" method="post" novalidate>
-              <div class="mb-3">
-                  <label for="inputIsim" class="form-label">Ad Soyad</label>
-                  <input type="text" class="form-control" id="inputIsim" name="isim">
-                  <p class="hataBilgisiIsim hataBilgisi mt-2"></p>
-              </div>
-              <div class="mb-3">
-                <label for="inputMail" class="form-label">Mail Adresi</label>
-                <input type="email" class="form-control" id="inputMail" name="mail">
-                <p class="hataBilgisiMail hataBilgisi mt-2"></p>
-              </div>
-              <div class="mb-3">
-                  <label for="inputSelect" class="form-label">Yaşadığınız Şehri Seçiniz</label>
-              <select id="inputSelect" class="form-select" name="sehir">
-                  <option selected value="İstanbul">İstanbul</option>
-                  <option value="Balıkesir">Balıkesir</option>
-                  <option value="Sivas">Sivas</option>
-              </select>
-              </div>
-              <div class="form-check">
-                  <input class="form-check-input" type="radio" name="mesajTuru" id="exampleRadios1" value="İstek ve öneri" checked>
-                  <label class="form-check-label" for="exampleRadios1">
-                      İstek ve öneri
-                  </label>
-              </div>
-              <div class="mb-3 form-check">
-                  <input class="form-check-input" type="radio" name="mesajTuru" id="exampleRadios2" value="Hata bildirimi">
-                  <label class="form-check-label" for="exampleRadios2">
-                    Hata bildirimi
-                  </label>
-              </div>
-              <div class="mb-2 form-group">
-                  <label for="inputText" class="form-label">Mesajınızı yazınız</label>
-                  <textarea class="form-control" id="inputText" name="mesaj" rows="3"></textarea>
-                  <p class="hataBilgisiMetin hataBilgisi mt-2"></p>
-              </div>
-              <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="inputCheckbox" required>
-                <label class="form-check-label" for="inputCheckbox">Bilgilerin doğruluğunu onaylıyorum.</label>
-                <p class="hataBilgisiCheckbox hataBilgisi mt-2" style="display: inline;"></p>
-              </div>
-              <button type="submit" class="btn btn-primary" id="submitButton">Gönder</button>
-              <button type="reset" class="btn btn-secondary">Temizle</button>   
-          </form>
-      </div>
-    </section>
+                      $hesapMail = 'b211210010@sakarya.edu.tr';
+                      $hesapSifre = 'b211210010';
+                      $girilenMail = $_POST['inputMail'];
+                      $girilenSifre = $_POST['inputPassword'];
+                      if($girilenMail == $hesapMail && $girilenSifre == $hesapSifre){
+                        echo '<p style = "text-align:center">Giriş işlemi başarılı.</p>';
+                        echo '<h2 class="fw-bold mb-2" style="text-align: center;">Hoşgeldiniz B211210010</h2>';
+                      }
+                      else{
+                        echo '<p class="text-italic" style = "text-align:center">Giriş işlemi başarısız.</p>';
+                        echo '<p class="fw-bold text-center yarrak mb-2">Kullanıcı bilgileri yanlış.</p>';
+                      }
+                    
+                    
+                    
+                    ?>
 
-    <script src="app.js"></script>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+   
 </body>
-    
 </html>
